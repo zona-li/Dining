e468cdd06dd3b7646bd8bd7e58f48753f3ffbfeb
Sophie Tao <sophietaoyn@gmail.com>
2017-04-19 23:53:27 +0000

    modified compose

--- app/exp/exp/settings.py
+++ app/exp/exp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f-yn9d%_!u3na$gtadsj26h27+&uhoby(y0*2w!3xziaf*ud+y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
--- app/web/web/settings.py
+++ app/web/web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u$eloij%%ro=3=dcfpce82x6kgk_*!#a*6+xdm$af+it#lx61t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
